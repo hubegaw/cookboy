2d1aa279b4a82e04fec6be2b93f9a61aa797833c
Hubert <hubert.gawczynski@gmail.com>
2023-03-02 14:37:47 +0100

    Initial commit

---
+++ src/main/java/com/cookboy/cookboy/CookboyApplication.java
@@ -0,0 +1,13 @@
+package com.cookboy.cookboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookboyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookboyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cookboy/cookboy/CookboyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookboy.cookboy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookboyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
